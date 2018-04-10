a733638b064e97c5fcf28320c86bd5468c563d99
sundar22894 <38243170+sundar22894@users.noreply.github.com>
2018-04-10 14:02:08 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,14 @@
+package program;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		int num;
+		for(num=0;num<=20;){num = num + 2;
+		System.out.println(num);
+		
+		}
+
+	}
+
+}
